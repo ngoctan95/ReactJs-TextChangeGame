b4ed5e943d7e0b2e50dc76fd5202fd30510191ae
APAC\GUT81HC <fixed-term.Tan.NguyenNgoc@vn.bosch.com>
2018-02-28 19:09:30 +0700

    build interface

--- src/App.js
+++ src/App.js
@@ -6,13 +6,39 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div class="container">
+          <div class="row">
+            <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+              <div class="panel panel-primary">
+                <div class="panel-heading" >
+                  <h3 class="panel-title" >Color Picker</h3>
+                </div>
+                <div class="panel-body">
+                </div>
+              </div>
+            </div>
+            <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+              <div class="panel panel-warning">
+                <div class="panel-heading">
+                  <h3 class="panel-title">Size: 15px</h3>
+                </div>
+                <div class="panel-body">
+                  <button type="button" class="btn btn-success">Tăng</button>
+                  <button type="button" class="btn btn-danger">Giảm</button>
+                </div>
+                <div>
+                  <button type="button" class="btn btn-primary warning">Reset</button>
+                </div>
+              </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" s>
+              <p>Nội dung cài đặt - Fontsize: 15px</p>
+              <div id="content" >
+                Nội dung cụ thể
+                </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
